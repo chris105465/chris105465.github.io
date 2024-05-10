4a21cb151e890e4c621e0b94003843abfa4e5292
chrisgrasser <s.grasser@students.clark.edu>
2024-05-09 17:15:55 -0700

    adjustments for accessibility

--- js/style.js
+++ js/style.js
@@ -27,7 +27,7 @@ function displayLayouts(css) {
     css.forEach(entry => {
         const card = document.createElement('div')
         card.classList = 'col-sm-12 col-md-6 col-lg-4 mb-3'
-        card.innerHTML = `<div class="card h-100"><img src="./img/${entry.category}/${entry.img}" class="card-img-top" alt="${entry.name} Layout Preview"><div class="card-body"><h5 class="card-title pb-1 ${entry.green}">${entry.name}</h5><textarea name="copy-field" class="copy-field" rows="4">&lt;link rel="stylesheet" type="text/css" href="${entry.link}" /></textarea></div></div>`
+        card.innerHTML = `<div class="card h-100"><img src="./img/${entry.category}/${entry.img}" class="card-img-top" alt="${entry.name} Layout Preview"><div class="card-body"><h5 class="card-title pb-1 ${entry.green}"><label for="${entry.name}Code">${entry.name}</label></h5><textarea name="copy-field" id="${entry.name}Code" class="copy-field" rows="4">&lt;link rel="stylesheet" type="text/css" href="${entry.link}" /></textarea></div></div>`
 
         if (entry.category == "aesthetic") {
             aesthetic.appendChild(card)
