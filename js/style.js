474fe0942974163f24f22fc2bd07848d302f0995
chris105465 <neighboringheart@hotmail.com>
2025-08-31 15:54:19 -0700

    further adjustments

--- js/style.js
+++ js/style.js
@@ -30,7 +30,7 @@ function displayLayouts(css) {
         card.classList = 'col-sm-12 col-md-6 col-lg-4 mb-3'
         let codeName = entry.name
         codeName = codeName.replace(/\s+/g, '')
-        card.innerHTML = `<div class="card h-100"><img src="./img/${entry.category}/${entry.img}" class="card-img-top" alt="${entry.name} Layout Preview"><div class="card-body"><h5 class="card-title pb-1 ${entry.green}"><div class="d-flex justify-content-between align-items-end"><label for="${codeName}Code">${entry.name}</label><button onclick="copyButton('${codeName}Code')" class="btn btn-primary">Copy Code</button></div></h5><textarea name="copy-field" id="${codeName}Code" class="copy-field" rows="5">&lt;link rel="stylesheet" type="text/css" href="${entry.link}" /></textarea></div></div>`
+        card.innerHTML = `<div class="card h-100"><img src="./img/${entry.category}/${entry.img}" class="card-img-top" alt="${entry.name} Layout Preview"><div class="card-body"><h5 class="card-title pb-1 ${entry.green}"><div class="d-flex justify-content-between align-items-end"><label for="${codeName}Code">${entry.name}</label><button onclick="copyButton('${codeName}Code')" class="btn btn-primary">Copy Code</button></div></h5><textarea name="copy-field" id="${codeName}Code" class="copy-field" rows="4">&lt;link rel="stylesheet" type="text/css" href="${entry.link}" /></textarea></div></div>`
 
         if (entry.category == "aesthetic") {
             aesthetic.appendChild(card)
